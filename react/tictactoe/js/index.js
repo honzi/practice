0c2062a105aebd5b86e904bcaacd2a9ef8f444d6
Jan Hořava <czechinthemail@gmail.com>
2019-03-18 19:03:03 -0700

    replaced null with _

--- react/tictactoe/js/index.js
+++ react/tictactoe/js/index.js
@@ -4,7 +4,7 @@ class Board extends React.Component{
   constructor(props){
       super(props);
       this.state = {
-        squares: Array(9).fill(null),
+        squares: Array(9).fill('_'),
       };
   }
 
